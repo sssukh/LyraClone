6b2600138b395541e449715945ec4a78e78527f6
sssukh <sssukh50@gmail.com>
2023-11-14 23:47:16 +0900

    Init Commit

---
+++ Source/LyraClone/LyraCloneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LyraCloneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LYRACLONE_API ALyraCloneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
